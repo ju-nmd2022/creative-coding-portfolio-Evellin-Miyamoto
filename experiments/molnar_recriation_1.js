4934bf392c33548ced786f30c4eee3b5729f6325
Evellin-Miyamoto <117274609+Evellin-Miyamoto@users.noreply.github.com>
2024-08-29 20:15:17 +0200

    small adjustments
    adjusting some values for better visualization.

--- experiments/molnar_recriation_1.js
+++ experiments/molnar_recriation_1.js
@@ -14,8 +14,7 @@ const squares = 6;
 const size = 90;
 
 function drawSquares() {
-  //colors
-  const colors = [
+  let colors = [
     color(255, 0, 0),
     color(255, 255, 0),
     color(0, 255, 0),
@@ -31,7 +30,8 @@ function drawSquares() {
 
   for (i = 0; i < 10; i++) {
     //to draw the grids
-    let s = 0; //square counter
+    //square counter
+    let s = 0;
     //nested loop
     for (y = 0; y < squares; y++) {
       for (x = 0; x < squares; x++) {
@@ -42,12 +42,12 @@ function drawSquares() {
         quad(
           random(-8, 10),
           random(-8, 10),
-          random(size / 2 - 6, size + 6),
+          random(size / 2 - 6, size + 8),
           random(-6, 6),
-          random(size / 2 - 6, size + 6),
-          random(size / 2 - 6, size + 6),
+          random(size / 2 - 6, size + 8),
+          random(size / 2 - 6, size + 8),
           random(-6, 6),
-          random(size / 2 - 6, size + 6)
+          random(size / 2 - 6, size + 8)
         );
         pop();
         s++;
@@ -57,7 +57,7 @@ function drawSquares() {
 }
 
 function draw() {
-  background(0);
+  background(30);
   noLoop();
   noFill();
   colorMode(RGB);
