c20a154282d92369020d41eae9da37671b9e2425
Evellin-Miyamoto <117274609+Evellin-Miyamoto@users.noreply.github.com>
2024-08-29 22:00:44 +0200

    molnar variation 1
    deleted the array and the for loop to create random colors. Added the noise to be on the random place.

--- experiments/molnar_variation_1.js
+++ experiments/molnar_variation_1.js
@@ -10,6 +10,8 @@ function setup() {
 }
 const squares = 6;
 const size = 90;
+const divider = 5;
+const counter = 0;
 
 function drawSquares() {
   let colors = [
@@ -32,8 +34,9 @@ function drawSquares() {
         push();
         //change the middle
         translate(width / 14 + x * size, height / 14 + y * size);
-
-        stroke(colors[s]);
+        let noiseValue = noise(x / divider, y / divider, counter); // Generates noise value between 0 and 1
+        let colorIndex = floor(map(noiseValue, 0, 1, 0, colors.length)); // Map noise to color index, floor to get a whole number
+        stroke(colors[colorIndex]);
         quad(
           random(-8, 10),
           random(-8, 10),
@@ -45,7 +48,7 @@ function drawSquares() {
           random(size / 2 - 6, size + 8)
         );
         pop();
-        s++;
+        counter += 0.05;
       }
     }
   }
@@ -53,7 +56,7 @@ function drawSquares() {
 
 function draw() {
   background(30);
-  noLoop();
+  //   noLoop();
   noFill();
   colorMode(RGB);
   drawSquares();
