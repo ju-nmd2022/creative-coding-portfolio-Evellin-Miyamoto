2cddec6eb517042bf5b45675d6c50641cc743054
Evellin-Miyamoto <117274609+Evellin-Miyamoto@users.noreply.github.com>
2024-08-30 23:23:42 +0200

    Tyler recreation
    The second recreation is inspired on Tyler Hobbs art with blue daisies. started by setting the daisy shape, the angle for the petals, grid and position

---
+++ experiments/tyler_recreation_1.js
@@ -0,0 +1,48 @@
+// Jönköping University
+// Creative Coding course - 2024
+// Evellin Miyamoto
+// Recreation of Tyler Hobbs
+
+// Based on class live coding by Bassima
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  background(235, 230, 230);
+}
+
+let flowerSize = 3;
+let amountOfDaisies = 7;
+let gap = 140;
+let petals = 5;
+let angle = 360 / petals;
+
+function daisy() {
+  for (let y = 0; y < petals; y++) {
+    for (let x = 0; x < petals; x++) {
+      noStroke();
+      fill(255, 255, 250);
+      ellipse(x, y, 10, 10);
+      fill(36, 83, 210);
+      rect(x, y, 8, 50);
+      rotate(angle);
+    }
+  }
+}
+
+function draw() {
+  let y =
+    (height - flowerSize * amountOfDaisies - gap * (amountOfDaisies - 1)) / 2;
+  for (let i = 0; i < amountOfDaisies; i++) {
+    let x =
+      (width - flowerSize * amountOfDaisies - gap * (amountOfDaisies - 1)) / 2;
+    for (let a = 0; a < amountOfDaisies; a++) {
+      push();
+      translate(x, y);
+      daisy();
+      pop();
+      x += flowerSize + gap;
+    }
+    y += flowerSize + gap;
+  }
+  noLoop();
+}
