9e738914de96af0ba231161016168ae5f871e3bf
Evellin-Miyamoto <117274609+Evellin-Miyamoto@users.noreply.github.com>
2024-09-01 12:38:55 +0200

    Taylor variation 1
    For this variation, I am making the daisies spin using Perlin Noise.

---
+++ experiments/tyler_variation_1.js
@@ -0,0 +1,59 @@
+// Jönköping University
+// Creative Coding course - 2024
+// Evellin Miyamoto
+// Recreation of Tyler Hobbs
+
+// Based on class live coding by Bassima
+// Based on Garrit class of Perlin Noise
+// Used the p5 reference https://p5js.org/reference/p5/noise/
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  background(235, 230, 230);
+  colorMode(HSB);
+  frameRate(1);
+}
+
+let flowerSize = 3;
+let amountOfDaisies = 7;
+let gap = 120;
+let petals = 5;
+let angle = 360 / petals;
+let noiseCounter = 0;
+
+function daisy() {
+  let saturation = random(20, 100);
+  for (let y = 0; y < petals; y++) {
+    for (let x = 0; x < petals; x++) {
+      noFill();
+      stroke(210, saturation, 75);
+      rect(x, y, 8, 50, 10);
+      rotate(random());
+    }
+  }
+}
+
+function draw() {
+  let y =
+    (height - flowerSize * amountOfDaisies - gap * (amountOfDaisies - 1)) / 2;
+  for (let i = 0; i < amountOfDaisies; i++) {
+    let x =
+      (width - flowerSize * amountOfDaisies - gap * (amountOfDaisies - 1)) / 2;
+    for (let a = 0; a < amountOfDaisies; a++) {
+      push();
+      translate(x, y);
+      daisy();
+      //perlin noise to make the daisies spin
+      let noiseValue = noise(noiseCounter);
+      let rotation = floor(map(noiseValue, 0, 1, 0));
+      //   rotate(rotation);
+      pop();
+      stroke(210, 75, 75);
+      fill(235, 0, 90);
+      ellipse(x, y, 23, 22);
+      x += flowerSize + gap;
+    }
+    y += flowerSize + gap;
+  }
+  // noLoop();
+}
