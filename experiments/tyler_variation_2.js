3b91dbe6c011d4df05576bebb70979609b4e9643
Evellin-Miyamoto <117274609+Evellin-Miyamoto@users.noreply.github.com>
2024-09-01 18:20:40 +0200

    Tyler variation 2
    For this variation 2, I want the daisies to change size with perlin noise and to change colors also using perlin noise.
    Started by the size element.

---
+++ experiments/tyler_variation_2.js
@@ -0,0 +1,63 @@
+// Jönköping University
+// Creative Coding course - 2024
+// Evellin Miyamoto
+// Variation 2 of Tyler Hobbs
+
+// Based on class live coding by Bassima
+// Based on Garrit class of Perlin Noise
+// Used the p5 reference https://p5js.org/reference/p5/noise/
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  colorMode(HSB);
+  frameRate(2);
+}
+
+let flowerSize = 3;
+let amountOfDaisies = 7;
+let gap = 120;
+let petals = 5;
+let noiseCounter = 0;
+
+function daisy(size) {
+  // let saturation = random(20, 100);
+  for (let y = 0; y < petals; y++) {
+    for (let x = 0; x < petals; x++) {
+      noFill();
+      stroke(210, saturation, 75);
+      rect(x * size, y * size, size * 3, size * 10, 10);
+      rotate(360);
+    }
+  }
+}
+
+function draw() {
+  background(240);
+  let y =
+    (height - flowerSize * amountOfDaisies - gap * (amountOfDaisies - 1)) / 2;
+  for (let i = 0; i < amountOfDaisies; i++) {
+    let x =
+      (width - flowerSize * amountOfDaisies - gap * (amountOfDaisies - 1)) / 2;
+    for (let a = 0; a < amountOfDaisies; a++) {
+      push();
+      translate(x, y);
+      //perlin noise to make the daisies spin
+      // let noiseValue = noise(noiseCounter);
+      // let rotation = floor(map(noiseValue, 0, 1, 0, PI));
+      // rotate(rotation);
+
+      //perlin noise to daisies change size
+      let noiseValueSize = noise(noiseCounter);
+      let flowerSizing = floor(map(noiseValueSize, 0, 1, 0, 5));
+      daisy(flowerSizing);
+
+      pop();
+      stroke(210, 75, 75);
+      fill(235, 0, 90);
+      ellipse(x, y, 23, 22);
+      x += flowerSize + gap;
+      noiseCounter += 0.1;
+    }
+    y += flowerSize + gap;
+  }
+}
