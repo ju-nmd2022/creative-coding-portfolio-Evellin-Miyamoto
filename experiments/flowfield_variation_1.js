8e4f343379c1bae1ecfc692354b319d3153399d6
Evellin-Miyamoto <117274609+Evellin-Miyamoto@users.noreply.github.com>
2024-09-02 18:19:08 +0200

    flow field variation 1
    This variation I changed the line to draw a rectangle, and changed some values to play around. The result is interesting and contrasting with the first approach.

---
+++ experiments/flowfield_variation_1.js
@@ -0,0 +1,63 @@
+// Jönköping University
+// Creative Coding course - 2024
+// Evellin Miyamoto
+// Recreation of Tyler Hobbs flowfield
+
+// Based on the Garrit's class
+// Adapted the code from this tutorial https://www.youtube.com/watch?v=To3dtYEQaf4&list=PLnJOmsprq3bE0QLbe7wZ8yb1-Dt0FBcP5&index=19
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  colorMode(HSB);
+  background(240, 0, 95);
+  stroke(5, 75, 100);
+  strokeWeight(3);
+  frameRate(4);
+}
+
+let borders = 0;
+let columnGrid = 50;
+let gridCellSize = (width - borders * 2) / columnGrid;
+let noiseResolution = 0.3;
+
+function lines() {
+  //grid
+  for (x = borders; x < width - borders + 1; x += gridCellSize) {
+    for (y = borders; y < height - borders + 1; y += gridCellSize) {
+      let oldX = x;
+      let oldY = y;
+      for (i = 0; i < 20; i++) {
+        //length of each small segment
+        let noiseSize = noise(oldX * noiseResolution, oldY * noiseResolution);
+        //segment length between 1% - 20% of the gridcellsize
+        let segmentLength = map(
+          noiseSize,
+          0,
+          1,
+          gridCellSize * 0.01,
+          gridCellSize * 0.5
+        );
+
+        //noise for direction
+        let noiseValue =
+          noise(oldX * noiseResolution, oldY * noiseResolution) + 0.03;
+        let angle = map(noiseValue, 0.4, 0.6, 0, PI * 2);
+
+        let newX = cos(angle) * segmentLength + oldX;
+        let newY = sin(angle) * segmentLength + oldY;
+
+        rect(oldX, oldY, newX, newY);
+
+        //update starting point for the next part
+        oldX = newX;
+        oldY = newY;
+        noiseResolution += 0.1;
+      }
+    }
+  }
+}
+
+function draw() {
+  //noLoop();
+  lines();
+}
