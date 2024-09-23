ba8363771db7fef85163b05b50130309ab557853
Evellin-Miyamoto <117274609+Evellin-Miyamoto@users.noreply.github.com>
2024-09-23 11:18:05 +0200

    Sound to spinning daisies
    in this variation, I am adding sound with tone.js to the spinning daisies, a variation of Tyler hobbs work inspiration. Started with getting sound based on the saturation of the daisies

---
+++ experiments/flowfield_sound.js
@@ -0,0 +1,73 @@
+// Jönköping University
+// Creative Coding course - 2024
+// Evellin Miyamoto
+// Adding sound to Tyler's recreation
+
+// Based on class live coding by Bassima
+// Based on Garrit class of Perlin Noise
+// Used the p5 reference https://p5js.org/reference/p5/noise/
+
+let synth;
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  background(235, 230, 230);
+  colorMode(HSB);
+  frameRate(2);
+  synth = new Tone.PolySynth().toDestination();
+}
+
+let flowerSize = 3;
+let amountOfDaisies = 7;
+let gap = 120;
+let petals = 5;
+let noiseCounter = 0;
+
+let notesC = ["C3", "D3", "E3", "G3", "A3"];
+let notesD = ["D5", "E5", "F5", "A5", "B5"];
+let notesE = ["E6", "F6♯", "G6♯", "B6", "C6♯"];
+
+function daisy() {
+  let saturation = random(20, 100);
+  //map saturation from 20 to 100 to the notes lenght
+  let noteIndex = floor(map(saturation, 20, 100, 0, notesC.length - 1));
+
+  synth.triggerAttackRelease(notesC[noteIndex], 0.3);
+  synth.triggerAttackRelease(notesD[noteIndex], 0.5);
+  synth.triggerAttackRelease(notesE[noteIndex], 1);
+
+  for (let y = 0; y < petals; y++) {
+    for (let x = 0; x < petals; x++) {
+      noFill();
+      stroke(210, saturation, 75);
+      rect(x, y, 8, 50, 10);
+      rotate(360);
+    }
+  }
+}
+
+function draw() {
+  background(240);
+  let y =
+    (height - flowerSize * amountOfDaisies - gap * (amountOfDaisies - 1)) / 2;
+  for (let i = 0; i < amountOfDaisies; i++) {
+    let x =
+      (width - flowerSize * amountOfDaisies - gap * (amountOfDaisies - 1)) / 2;
+    for (let a = 0; a < amountOfDaisies; a++) {
+      push();
+      translate(x, y);
+      //perlin noise to make the daisies spin
+      let noiseValue = noise(noiseCounter);
+      let rotation = floor(map(noiseValue, 0, 1, 0, PI));
+      rotate(rotation);
+      daisy();
+      pop();
+      stroke(210, 75, 75);
+      fill(235, 0, 90);
+      ellipse(x, y, 23, 22);
+      x += flowerSize + gap;
+      noiseCounter += 0.1;
+    }
+    y += flowerSize + gap;
+  }
+}
